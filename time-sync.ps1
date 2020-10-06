34f9d9d0bd4ab0fe692c3067888d967c1f664fdb
Michael Dryden <5611490+mdryden@users.noreply.github.com>
2020-10-06 11:37:29 -0500

    Update time-sync.ps1

--- time-sync.ps1
+++ time-sync.ps1
@@ -1,6 +1,5 @@
 # Re-syncs docker VM and windows host time caused by system sleep
-# You might need to run this in an admin powershell prompt:
-# Set-ExecutionPolicy RemoteSigned
+# You might need to run this in an admin powershell prompt
 
 Get-VMIntegrationService -VMName "DockerDesktopVM" -Name "Time Synchronization" | Disable-VMIntegrationService
 Get-VMIntegrationService -VMName "DockerDesktopVM" -Name "Time Synchronization" | Enable-VMIntegrationService
