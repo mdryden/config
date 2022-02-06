bab1392c46eaeec1cb1729077d464816244a5f52
Michael Dryden <5611490+mdryden@users.noreply.github.com>
2022-02-06 12:06:01 -0600

    Create arch-setup.sh

---
+++ arch-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# untested, just notes at this point.
+
+# yay -S gnome-terminal
+# yay -S visual-studio-code-bin
+# yay -S ttf-ms-win10-auto
+# yay -S nerd-fonts-complete
+# yay -S google-chrome
