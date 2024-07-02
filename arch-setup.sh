785673ccfb3ae98e0f29009fe2f4cd3d6634aced
Michael Dryden <5611490+mdryden@users.noreply.github.com>
2024-07-01 20:28:23 -0500

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -2,8 +2,25 @@
 
 # untested, just notes at this point.
 
-# yay -S gnome-terminal
-# yay -S visual-studio-code-bin
-# yay -S ttf-ms-win10-auto
-# yay -S nerd-fonts-complete
-# yay -S google-chrome
+yay -S gnome-terminal
+yay -S visual-studio-code-bin
+yay -S ttf-ms-win10-auto
+yay -S nerd-fonts-complete
+yay -S google-chrome
+
+echo "Installing starship"
+curl -sS https://starship.rs/install.sh | sh
+
+echo "Configuring starship"
+mkdir -p ~/.config
+curl https://raw.githubusercontent.com/mdryden/config/master/shell_profiles/starship.toml -o ~/.config/starship.toml
+
+echo "Installing thefuck"
+PIP_REQUIRE_VIRTUALENV=false pip3 install thefuck
+
+echo "Downloading and installing .mikerc"
+curl https://raw.githubusercontent.com/mdryden/config/master/shell_profiles/.mikerc -o ~/.mikerc
+grep -qxF 'source "$HOME/.mikerc"' $HOME/.profile || printf '\nsource "$HOME/.mikerc"' >> $HOME/.profile
+
+echo "Enabling new profile"
+source ~/.profile
